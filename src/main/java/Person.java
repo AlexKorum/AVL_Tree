958a8ba38a190800438f1954bcef6be434284d7b
Korum <alex.korum@mail.ru>
2019-01-29 22:56:51 +0300

    Initial commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+    private int ID;
+
+    public Person(int ID) {
+        this.ID = ID;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+}
